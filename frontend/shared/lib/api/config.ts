67e1402731a60819982c1f2dbf3e09cb680e95fa
Kevin <tolbert.k@northeastern.edu>
2025-12-07 16:05:57 -0500

    asdasd

--- frontend/shared/lib/api/config.ts
+++ frontend/shared/lib/api/config.ts
@@ -1,2 +1,6 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000/api";
-export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://127.0.0.1:8000";
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../.env' });
+
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
