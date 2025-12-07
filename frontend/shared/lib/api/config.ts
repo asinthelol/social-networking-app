dcd201b637c95382712cf2766d805ac19f16d40a
Kevin <tolbert.k@northeastern.edu>
2025-12-07 16:35:21 -0500

    dddd

--- frontend/shared/lib/api/config.ts
+++ frontend/shared/lib/api/config.ts
@@ -1,6 +1,2 @@
-import dotenv from 'dotenv';
-
-dotenv.config({ path: '../.env' });
-
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
